cfb1a91f417ee63095c267f0cc340672c68fcabd
OneCrazyman <cjw7155@naver.com>
2024-07-04 01:26:24 +0900

    add: for keymap-editor

---
+++ config/chippy.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define U_TAPPING_TERM 200
+
+#if defined (MIRYOKU_KLUDGE_MOUSEKEYSPR)
+  #include "miryoku_kludge_mousekeyspr.h"
+#else
+  #include "miryoku_mousekeys.h"
+#endif
+#include "miryoku_behaviors.h"
