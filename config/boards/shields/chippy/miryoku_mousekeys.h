f400b11f6259675226867b955ea364d98247d4cd
OneCrazyman <cjw7155@naver.com>
2024-07-04 20:02:02 +0900

    #

--- config/boards/shields/chippy/miryoku_mousekeys.h
+++ config/boards/shields/chippy/miryoku_mousekeys.h
@@ -7,10 +7,10 @@
 #define U_BTN2 &mkp MB2
 #define U_BTN3 &mkp MB3
 
-#define U_MS_D &macro ctrl_kp_n2
-#define U_MS_L &macro ctrl_kp_n4
-#define U_MS_R &macro ctrl_kp_n6
-#define U_MS_U &macro ctrl_kp_n8
+#define U_MS_L &ctrl_kp_n4
+#define U_MS_D &ctrl_kp_n4
+#define U_MS_R &ctrl_kp_n6
+#define U_MS_U &ctrl_kp_n8
 #define U_WH_D &none
 #define U_WH_L &none
 #define U_WH_R &none
