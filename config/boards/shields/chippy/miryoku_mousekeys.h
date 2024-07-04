71efdd1b7b351348a56cc228194c20d67f3a6849
OneCrazyman <cjw7155@naver.com>
2024-07-05 04:24:14 +0900

    fix: U_MS_KEYS

--- config/boards/shields/chippy/miryoku_mousekeys.h
+++ config/boards/shields/chippy/miryoku_mousekeys.h
@@ -7,10 +7,10 @@
 #define U_BTN2 &mkp MB2
 #define U_BTN3 &mkp MB3
 
-#define U_MS_L &msl
-#define U_MS_D &msd
-#define U_MS_R &kp KP_N6
-#define U_MS_U &kp KP_N8
+#define U_MS_L &kp LC(KP_N4)
+#define U_MS_D &kp LC(KP_N2)
+#define U_MS_R &kp LC(KP_N6)
+#define U_MS_U &kp LC(KP_N8)
 #define U_WH_D &none
 #define U_WH_L &none
 #define U_WH_R &none
